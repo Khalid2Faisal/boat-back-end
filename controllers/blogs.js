3e7fe658d5a6d3c4b186c5cd640eefa8c83d1ab2
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2022-07-16 17:57:11 +0200

    reduce accepted image size to 1mb

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -59,9 +59,9 @@ const create = (req, res) => {
     let arrayOfTags = tags && tags.split(",");
 
     if (files.photo) {
-      if (files.photo.size > 10000000) {
+      if (files.photo.size > 1000000) {
         return res.status(400).json({
-          error: "Image should be less than 10mb in size",
+          error: "Image should be less than 1mb in size",
         });
       }
       blog.photo.data = fs.readFileSync(files.photo.path);
